ded652dcf4827e9ffc74c6a377cfabdc6d7b04f1
Baris <baris@abc.com>
2020-12-13 15:46:06 +0100

    RegisterPage, Register_Steps and Register feature files has been added

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -20,11 +20,11 @@ import org.junit.runner.RunWith;
 //        tags = "@berati",
 //        tags = "@baris",
 //        tags = "@leyla",
-        tags = "@US11",
+//        tags = "@US11",
 //        tags = "@tugba",
-        tags = "@deniz",
+//        tags = "@deniz",
 //        tags = "@fatih",
-
+        tags = "@US_03",
         dryRun = false
 )
 public class Runner {
---
+++ src/test/java/stepdefinitions/Register_Steps.java
@@ -0,0 +1,58 @@
+package stepdefinitions;
+
+import pages.RegisterPage;
+import utilities.Driver;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Register_Steps {
+    RegisterPage registerPage = new RegisterPage();
+
+    @Given("go to homepage and click user icon")
+    public void goToHomepageAndClickUserIcon() {
+        Driver.getDriver().get("http://gmibank.com/");
+        registerPage.accountMenu.click();
+    }
+
+    @Given("click register page")
+    public void clickRegisterPage() {
+        registerPage.registerButton.click();
+    }
+
+    @When("edit password field with {int} char type variety" )
+    public void editPasswordFieldWithCharTypeVariety(int charTypeVariety, DataTable table) {
+        List<String> passwords = table.asList(String.class);
+        List<WebElement> cells = registerPage.strengthBarCells;
+
+        String[] colors = { "background-color: rgb(255, 0, 0);", //red
+                            "background-color: rgb(255, 153, 0);", // orange with 2 different char type
+                            "background-color: rgb(153, 255, 0);", // light green with 3 different char type
+                            "background-color: rgb(0, 255, 0);", // dark green with 4 different char type
+                            "background-color: rgb(221, 221, 221);" // gray
+                          };
+
+        for(String each : passwords){
+            Arrays.stream(each.split("")).forEach(ch->registerPage.password.sendKeys(ch));
+            Driver.wait(1);
+            Boolean colorCode = IntStream.range(0, charTypeVariety)
+                                         .allMatch(i -> cells.get(i).getAttribute("style")
+                                                                    .equals(colors[charTypeVariety - 1]));
+
+            Assert.assertTrue(colorCode);
+            Driver.wait(1);
+            registerPage.password.clear();
+
+
+        }
+
+    }
+
+
+}
