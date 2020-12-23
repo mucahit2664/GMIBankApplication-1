87e4b2b185594c554e576bf2a5e28ec5d2cd0ecd
Deniz Ozyurt <denizozyurt027@gmail.com>
2020-12-23 18:45:38 +0100

    23.12.2020lastVersion

---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,32 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        strict = true,
+        plugin = {
+                "html:target/default-cucumber-reports",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"
+        },
+        //plugin = "html:target\\default-cucumber-reports",//for windows
+        features ="src/test/resources/features",
+        glue = "stepdefinitions",
+        //       tags = "@omer",
+        tags = "@mahmut",
+//        tags = "@berati",
+//        tags = "@baris",
+//        tags = "@leyla",tcO1,tc02,tc03,tc04,tc05,loginemployeeAndus10 //tc06,tc07,tc08,tc09, logincustomerAndus15
+//        tags = "@US11",
+//        tags = "@tugba",
+//        tags = "@deniz",
+//        tags = "@fatih",
+//        tags = "@US_03",
+//        tags = "@US_13",
+        dryRun = false
+)
+public class Runner {
+}
