d22a45bcf0aa70faae163e39ad938333c1a20275
maliayyildiz <you@example.com>
2020-12-24 19:00:08 +0100

    methods are added in pojos

--- src/test/java/utilities/WriteToTxt.java
+++ src/test/java/utilities/WriteToTxt.java
@@ -19,25 +19,7 @@ public class WriteToTxt {
         }
     }
 
-    public static void saveDataInFileWithAllData(String fileName, Customer[] customers){
-        try{
-            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName,true));
 
-            for (int i = 0; i< customers.length; i++){
-                writer.append(customers[i].getFirstName()+" , "+customers[i].getLastName()+"\n");
-
-                if(customers[i].getUser() != null)
-                    writer.append(customers[i].getUser().getFirstName());
-                if (customers[i].getCountry() != null)
-                    writer.append(customers[i].getCountry().getName());
-
-            }
-
-            writer.close();
-        }catch (Exception e){
-
-        }
-    }
 
     public static void saveAllStates(String fileName, State[] states){
         try{
